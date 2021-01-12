636b9c58404ddd8b37fa24ae2517d5f4f35472f6
denise-khuu <74537227+denise-khuu@users.noreply.github.com>
2021-01-12 17:14:10 +0100

    finished cobigen-cli

--- cobigen-cli/files/Placeholder.java
+++ cobigen-cli/files/Placeholder.java
@@ -1,3 +1,13 @@
 private int age;
+
 private String company;
 
+/** @return the company */
+public String getCompany() {
+    return company;
+  }
+  
+  /** @param company the company to set */
+  public void setCompany(String company) {
+    this.company = company;
+  }
