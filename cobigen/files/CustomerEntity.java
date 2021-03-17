f4d637088c05d570dba8a8ef2c439bdc40714efa
Marcel Dießner <marcel.diessner@freenet.de>
2021-03-17 16:03:51 +0100

    Readded cobigen files

---
+++ cobigen/files/CustomerEntity.java
@@ -0,0 +1,60 @@
+package com.example.application.cobigenexample.customermanagement.dataaccess.api;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Customer")
+public class CustomerEntity {
+
+  private String firstname;
+
+  private String lastname;
+
+  private int age;
+
+  /**
+   * @return the firstname
+   */
+  public String getFirstname() {
+    return firstname;
+  }
+
+  /**
+   * @param firstname the firstname to set
+   */
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  /**
+   * @return the lastname
+   */
+  public String getLastname() {
+    return lastname;
+  }
+
+  /**
+   * @param lastname the lastname to set
+   */
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  /**
+   * @return the age
+   */
+  public int getAge() {
+    return age;
+  }
+
+  /**
+   * @param age the age to set
+   */
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+}
