aa0c314e5c68e37d822d7a987029a71c81e0b8ed
dixyushi <you@example.com>
2021-05-28 03:47:09 +0530

    working rest api added

---
+++ devon4j-http-rest-client/files/V0006__Master_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (0, 1, 'mike@mail.com', 'mike@123');
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (1, 1, 'tanya@mail.com', 'tanya@123');
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (2, 1, 'anya@mail.com', 'anya@123');
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (3, 1, 'vanya@mail.com', 'vanya@123');
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (4, 1, 'sanya@mail.com', 'sanya@123');
+INSERT INTO Visitor (id, modificationCounter, username, password) VALUES (5, 1, 'manya@mail.com', 'manya@123');
