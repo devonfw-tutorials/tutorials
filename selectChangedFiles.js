91ab906e7bfe1cc91f089833a43a47ba0e308c72
Marcel Dießner <marcel.diessner@freenet.de>
2021-06-11 13:03:56 +0200

    removed debug

--- selectChangedFiles.js
+++ selectChangedFiles.js
@@ -8,20 +8,18 @@ async function selectChangedFiles() {
     try {
         let get = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
             //owner: 'devonfw-tutorials',
-            owner: 'MarcelDiessner',
+            owner: 'devonfw-tutorials',
             repo: 'tutorials',
-            pull_number: 41
+            pull_number: pr
         });
         let files = get.data;
         files.forEach(file => {
             arr.push(file.filename)
         });
-        console.log(arr);
     } catch(e) {
         throw e;
     }
     let output = arr.join(' ');
-    console.log(output);
     core.info(`Changed Files: ${output}`);
     core.setOutput('changedFiles', output);
 }
