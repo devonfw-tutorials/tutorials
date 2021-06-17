69113fccb2ef3f860046921fa36266e2f3fa3003
dixyushi <you@example.com>
2021-06-17 11:43:34 +0530

    modified files and tutorial

---
+++ devon4j-http-rest-client/clientfile/VisitormanagementRestService.java
@@ -0,0 +1,19 @@
+package com.sample.application.httprestclient.visitormanagement.service.api.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/visitormanagement")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface VisitormanagementRestService {
+
+  @GET
+  @Path("/clientrequest")
+  public String returnResponseToClient();
+
+}
