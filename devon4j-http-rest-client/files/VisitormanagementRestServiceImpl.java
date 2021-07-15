950f301df1e80a045c7ac9c3537d39e40a40c4c6
Sarmokadam <sneha.a.sarmokadam@capgemini.com>
2021-07-15 17:02:37 +0530

    modifying tutorial to fix errors

--- devon4j-http-rest-client/files/VisitormanagementRestServiceImpl.java
+++ devon4j-http-rest-client/files/VisitormanagementRestServiceImpl.java
@@ -1,16 +1,16 @@
-package com.example.application.httprestserver.visitormanagement.service.impl.rest;
+package com.example.application.visitormanagement.service.impl.rest;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 
-import com.example.application.httprestserver.visitormanagement.service.api.rest.VisitormanagementRestService;
+import com.example.application.visitormanagement.service.api.rest.VisitormanagementRestService;
 
 @Named("VisitormanagementRestService")
 public class VisitormanagementRestServiceImpl implements VisitormanagementRestService {
 
   @Override
   public String returnResponseToClient() {
-   String args = "welcome to rest api";
-   return args;
+
+    return "Welcome to REST API world";
   }
+
 }
